f1d208fa0240e7ef9c5e8aead17d90083f15e434
Praqma Release User <man@praqma.net>
2018-04-13 11:47:27 +0200

    Added small Library method to get a project back from SDElements. close #5

--- src/main/java/io/jenkins/plugins/sdelements/api/SDElementsLibrary.java
+++ src/main/java/io/jenkins/plugins/sdelements/api/SDElementsLibrary.java
@@ -1,10 +1,35 @@
 package io.jenkins.plugins.sdelements.api;
 
-/**
- * Placeholder package for a possible API
- */
-public class SDElementsApi {
-    public boolean riskIndication() {
-        return true;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import java.util.HashMap;
+
+public class SDElementsLibrary {
+
+    private String accessKey;
+    private String url;
+    private String apiVersion = "v2";
+
+    public SDElementsLibrary(String accessKey, String url) {
+        this.accessKey = accessKey;
+        this.url = url;
+    }
+
+    public HttpResponse<JsonNode> getProject(int id) throws UnirestException {
+        String projects = url + "/api/" + apiVersion + "/projects/";
+        System.out.println(projects);
+        HashMap<String,String> headers = new HashMap<>();
+        headers.put("Accept", "application/json");
+        headers.put("Authorization","Token "+accessKey);
+        HttpResponse<JsonNode> resp = Unirest.get(projects).
+                headers(headers).
+                queryString("application", id).asJson();
+        return resp;
+    }
+
+    public void setApiVersion(String apiVersion) {
+        this.apiVersion = apiVersion;
     }
 }
