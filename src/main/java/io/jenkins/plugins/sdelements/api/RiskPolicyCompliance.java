03f5b932b40b89333b8f596a8e445420e207c64a
Praqma Release User <man@praqma.net>
2018-04-18 14:27:01 +0200

    Provide better names for the enum. Related to #5

--- src/main/java/io/jenkins/plugins/sdelements/api/RiskPolicyCompliance.java
+++ src/main/java/io/jenkins/plugins/sdelements/api/RiskPolicyCompliance.java
@@ -4,7 +4,22 @@ package io.jenkins.plugins.sdelements.api;
  * Created by mads on 4/13/18.
  */
 public enum RiskPolicyCompliance {
-    PASS, //In case of survey compliance
-    FAIL, //In case of survey failure
-    UNDETERMINED //In case of incomplete survey
+    PASS("Pass"), //In case of survey compliance
+    FAIL("Fail"), //In case of survey failure
+    UNDETERMINED("Survey not completed"); //In case of incomplete survey
+
+    private String displayName;
+
+    RiskPolicyCompliance(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @Override
+    public String toString() {
+        return displayName;
+    }
 }
